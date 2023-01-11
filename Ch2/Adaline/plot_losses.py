b54c942ff885b3a1ca49bbbbcd1c7a7ad5533f11
Adam Martin <adammartin2019@gmail.com>
2023-01-10 20:24:46 -0500

    graphing works properly

--- Ch2/Adaline/plot_losses.py
+++ Ch2/Adaline/plot_losses.py
@@ -3,31 +3,33 @@ import numpy as np
 import pandas as pd
 from adaline import AdalineGD
 
-if __name__ == "__main__":
-    data = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-    df = pd.read_csv(data, header=None, encoding='utf-8')
 
-    y = df.iloc[0:100, 4].values
-    y = np.where(y == 'Iris-setosa', 0, 1)
+data = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(data, header=None, encoding='utf-8')
 
-    X = df.iloc[0:100, [0,2]].values
-    
-    #plot data
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,4))
-    
-    ada1 = AdalineGD(n_iter=15, eta=.1).fit(X,y)
-    ax[0].plot(range(1, len(ada1.losses_) + 1),
-               np.log10(ada1.losses_), marker='o')
-    ax[0].set_xlabel('Epochs')
-    ax[0].set_ylabel('log(Mean squared error)')
-    ax[0].set_title('Adaline - Learning rate .1')
-    
-    ada2 = AdalineGD(n_iter=15, eta=.0001).fit(X,y)
-    ax[1].plot(range(1, len(ada2.losses_) + 1),
-               ada2.losses_, marker='o')
-    ax[1].set_xlabel('Epochs')
-    ax[1].set_ylabel('log(Mean squared error)')
-    ax[1].set_title('Adaline - Learning rate .0001')
-    
-    plt.show()
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', 0, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+#plot data
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = AdalineGD(n_iter=15, eta=0.1).fit(X, y)
+ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Mean squared error)')
+ax[0].set_title('Adaline - Learning rate 0.1')
+
+ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) + 1), ada2.losses_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Mean squared error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+# plt.savefig('images/02_11.png', dpi=300)
+plt.show()
+
     
--- Ch2/Adaline/standarized_training.py
+++ Ch2/Adaline/standarized_training.py
@@ -10,56 +10,60 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     markers = ('o', 's', '^', 'v', '<')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
-    
+
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-    np.arange(x2_min, x2_max, resolution))
+                           np.arange(x2_min, x2_max, resolution))
     lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     lab = lab.reshape(xx1.shape)
     plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
+
     # plot class examples
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0],
-        y=X[y == cl, 1],
-        alpha=0.8,
-        c=colors[idx],
-        marker=markers[idx],
-        label=f'Class {cl}',
-        edgecolor='black')
- 
- 
- 
-if __name__ == "__main__":
-    data = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-    df = pd.read_csv(data, header=None, encoding='utf-8')
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=f'Class {cl}', 
+                    edgecolor='black')
+        
 
-    y = df.iloc[0:100, 4].values
-    y = np.where(y == 'Iris-setosa', 0, 1)
+data = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(data, header=None, encoding='utf-8')
 
-    X = df.iloc[0:100, [0,2]].values
-    
-    #standarize data
-    X_std = np.copy(X)
-    X_std[:,0] = (X[:,0].mean()) / X[:,0].std()
-    X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
-    
-    ada_gd = AdalineGD(n_iter=20, eta=.5)
-    ada_gd.fit(X_std, y)
-    print("finished fitting model")
-    plot_decision_regions(X_std, y, classifier=ada_gd)
-    plt.title('Adaline - Gradient descent')
-    plt.xlabel('Sepal length [standardized]')
-    plt.ylabel('Petal length [standardized]')
-    plt.legend(loc='upper left')
-    plt.tight_layout()
-    plt.show()
-    plt.plot(range(1, len(ada_gd.losses_) + 1),
-                ada_gd.losses_, marker='o')
-    plt.xlabel('Epochs')
-    plt.ylabel('Mean squared error')
-    plt.tight_layout()
-    plt.show()
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', 0, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# standardize features
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada_gd = AdalineGD(n_iter=20, eta=0.5)
+ada_gd.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada_gd)
+plt.title('Adaline - Gradient descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('images/02_14_1.png', dpi=300)
+plt.show()
+
+plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Mean squared error')
+
+plt.tight_layout()
+#plt.savefig('images/02_14_2.png', dpi=300)
+plt.show()
